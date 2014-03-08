9ace0cfe02e853cbf553471126f224ee284d3986
elliottcordo <elliottcordo@gmail.com>
2014-03-08 12:54:08 -0500

    initial files and commands

---
+++ mahout_cmds/itemsimilarity.sh
@@ -0,0 +1,7 @@
+#will make input params later
+#remove files
+hadoop fs -rmr /user/movie_lens_rec_item_similarity 
+hadoop fs -rmr /app/hadoop/tmp/recommenditemsimilarity
+#run hadoop
+/usr/local/hadoop/mahout/bin/mahout itemsimilarity --input /user/movie_lens_data --output /user/movie_lens_rec_item_similarity --tempDir /app/hadoop/tmp/recommenditemsimilarity --similarityClassname SIMILARITY_COSINE
+
