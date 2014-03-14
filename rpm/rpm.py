d7143561914ec394b9658cbb5d70c183e236a14d
elliottcordo <elliottcordo@gmail.com>
2014-03-14 16:37:59 -0400

    fixed 500 for item or user not found, added ordered dict result

--- rpm/rpm.py
+++ rpm/rpm.py
@@ -1,4 +1,5 @@
 #py imports
+from collections import OrderedDict
 from flask import Flask
 import redis
 
@@ -20,27 +21,46 @@ def index():
     return 'rpM Recommender :)'
 
 #recommendation getter
-@app.route('/recommender/<int:itemid>/<string:userid>')
-def recommender (itemid,userid):
+@app.route('/recommender/<string:userid>/<int:itemid>')
+def recommender (userid, itemid):
 
     #get from redis, no pipeline, it's just two calls (for now)
-    itemsim = rP.zrevrange(itemid, 0, 9 ,'withscores')
-    itembase = rP.zrevrange('U-'+userid,0, 9, 'withscores')
+    itemsim = rP.zrevrange(itemid, 0, 25 ,'withscores')
+    itembase = rP.zrevrange('U-'+userid,0, 25, 'withscores')
 
     #get top scores so we can level the two datasets (item base is 1 to 5, item sim 0 to 1)
-    topitemsim = itemsim[0][1]
-    topitembase = itembase[0][1]
+
+    if  itemsim and itembase:
+        topitemsim = itemsim[0][1]
+        topitembase = itembase[0][1]
+        factor = topitembase/topitemsim
+    else:
+        factor = 1
 
     #next we'll spin these lists into a results dict, toping and adding scores
+    dictRes = dict()
+
+    for i in itembase:
+        dictRes[i[0]] = i[1]
+
+    for i in itemsim:
+        if i[0] in dictRes:
+            dictRes[i[0]] = dictRes[i[0]] + i[1]*factor
+        else:
+            dictRes[i[0]] = i[1]*factor
 
     #then we'll sort the dict
-
-    #return resutls - for now just the raw recommendations, but later from the dict
+    dictResS = OrderedDict(sorted(dictRes.items(), key=lambda t: t[1], reverse=True))
+    #return results - for now just the raw recommendations, but later from the dict
     #maybe a nice json representation or html page
-    results = '\nitem_base:' + str(itembase) + ' \nitem_sim:' + str(itemsim)
 
-    return results
+    # great use case: http://127.0.0.1:5000/recommender/7/572
+    if cfg.g('debug','terminal') == '1':
+        print '----\norig item_base:' + str(itembase)
+        print '----\norig item_sim:' + str(itemsim)
+        print '----\ncombined result:' + str(dictResS)
 
+    return  str(dictResS)
 
 if __name__ == '__main__':
     app.run()
