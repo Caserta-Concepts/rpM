33f9b68bdd66dd66b8de5164377b77f24828ba58
elliottcordo <elliottcordo@gmail.com>
2014-03-14 18:32:54 -0400

    got json working, added numitems param

--- rpm/rpm.py
+++ rpm/rpm.py
@@ -1,6 +1,6 @@
 #py imports
 from collections import OrderedDict
-from flask import Flask
+from flask import Flask, jsonify
 import redis
 
 #project imports
@@ -21,12 +21,15 @@ def index():
     return 'rpM Recommender :)'
 
 #recommendation getter
-@app.route('/recommender/<string:userid>/<int:itemid>')
-def recommender (userid, itemid):
+@app.route('/recommender/<string:userid>/<int:itemid>/<int:numitems>')
+def recommender (userid, itemid, numitems):
+
+    #we will retrieve twice the requested recommendations from each set
+    numperset = numitems*2
 
     #get from redis, no pipeline, it's just two calls (for now)
-    itemsim = rP.zrevrange(itemid, 0, 25 ,'withscores')
-    itembase = rP.zrevrange('U-'+userid,0, 25, 'withscores')
+    itemsim = rP.zrevrange(itemid, 0, numperset ,'withscores')
+    itembase = rP.zrevrange('U-'+userid,0,  numperset, 'withscores')
 
     #get top scores so we can level the two datasets (item base is 1 to 5, item sim 0 to 1)
 
@@ -49,18 +52,20 @@ def recommender (userid, itemid):
         else:
             dictRes[i[0]] = i[1]*factor
 
-    #then we'll sort the dict
-    dictResS = OrderedDict(sorted(dictRes.items(), key=lambda t: t[1], reverse=True))
-    #return results - for now just the raw recommendations, but later from the dict
-    #maybe a nice json representation or html page
+    #then we'll sort the dict and take the top results based on numitems
+    dictResS = OrderedDict(sorted(dictRes.items(), key=lambda t: t[1], reverse=True)[:numitems])
 
-    # great use case: http://127.0.0.1:5000/recommender/7/572
+
+    #return results
+    # great use case: http://127.0.0.1:5000/recommender/572/7/25
     if cfg.g('debug','terminal') == '1':
         print '----\norig item_base:' + str(itembase)
         print '----\norig item_sim:' + str(itemsim)
         print '----\ncombined result:' + str(dictResS)
 
-    return  str(dictResS)
+    results = jsonify(recommendations=dictResS)
+    #results = str(dictResS)
 
+    return results
 if __name__ == '__main__':
     app.run()
